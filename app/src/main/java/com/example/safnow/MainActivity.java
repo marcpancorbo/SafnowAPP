4b0e62eb4f3a894f034e8cd989d5ffc3eca93e49
marcpancorbo <55668102+marcpancorbo@users.noreply.github.com>
2020-03-10 19:28:24 +0100

    Tab navigator hecho

--- app/src/main/java/com/example/safnow/MainActivity.java
+++ app/src/main/java/com/example/safnow/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.safnow;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout drawerLayout;
+public class MainActivity extends AppCompatActivity  {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-
+        setContentView(R.layout.navigator);
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        TabItem tabContacts = findViewById(R.id.tabContacts);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
     }
 
@@ -30,7 +38,4 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
-
-
-
 }
