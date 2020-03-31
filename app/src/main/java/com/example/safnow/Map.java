a8b4bff0f4cdfd6e06916a98d647540ffb73e59c
Marc Marza <marcmarzaosa@gmail.com>
2020-03-31 13:43:28 +0200

    Activity map realizada

---
+++ app/src/main/java/com/example/safnow/Map.java
@@ -0,0 +1,26 @@
+package com.example.safnow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class Map extends Fragment {
+
+    public Map() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_maps, container, false);
+    }
+}
--- app/src/main/java/com/example/safnow/PageAdapter.java
+++ app/src/main/java/com/example/safnow/PageAdapter.java
@@ -1,5 +1,7 @@
 package com.example.safnow;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,19 +10,22 @@ import androidx.fragment.app.FragmentPagerAdapter;
 public class PageAdapter extends FragmentPagerAdapter {
     private int numOfTabs;
 
-    public PageAdapter(FragmentManager fm, int numOfTabs){
+    public PageAdapter(FragmentManager fm, int numOfTabs) {
         super(fm);
         this.numOfTabs = numOfTabs;
     }
+
     @NonNull
     @Override
     public Fragment getItem(int position) {
+        Log.d("marc","NumOfTabs" + position);
+
         switch (position) {
             case 0:
                 return new Contact();
             case 1:
-                return new Contact();
-                case 2:
+                return new Map();
+            case 2:
                 return new Contact();
             default:
                 return null;
