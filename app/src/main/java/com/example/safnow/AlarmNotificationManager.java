eeb69d61574b88bc864660a429fa3368248112fe
Luis Servera <lservera03@gmail.com>
2020-05-17 20:50:21 +0200

    Implementada funcionalidad de alarma y gestión de las notificaciones.

--- app/src/main/java/com/example/safnow/AlarmNotificationManager.java
+++ app/src/main/java/com/example/safnow/AlarmNotificationManager.java
@@ -3,14 +3,11 @@ package com.example.safnow;
 import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
-import android.util.Log;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
